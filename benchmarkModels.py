5a594866820e4270427cb4c7711a999124a3fe9e
Thea Aarrestad <thea.aarrestad@cern.ch>
2020-10-16 15:07:14 +0200

    adding trace

--- benchmarkModels.py
+++ benchmarkModels.py
@@ -27,38 +27,54 @@ def print_dict(d, indent=0):
 
 def toHLS(model,precision=32):
   (x_train, y_train), (x_test, y_test) = getNumpyData('svhn_cropped',oneHot=False)
-  #hls_cfg = hls4ml.utils.config_from_keras_model(m,granularity='type')
-  #hls_cfg['Model'] = {}
+  
+  #get model precision
   pw = 'ap_fixed<{},{}>'.format(precision,intbits_w)
   po = 'ap_fixed<{},{}>'.format(precision,intbits_a) 
   if precision < 10:
     pw = 'ap_fixed<{},{}>'.format(precision,4)
     po = 'ap_fixed<{},{}>'.format(precision,4)  
   
-  hls_cfg = {'Model' : {'Precision' : po}}
- # hls_cfg['LayerName'] = {'output_softmax' : {'Strategy' : 'Stable'}}
-#  hls_cfg['LayerType'] = {'Input' : {'Precision' : 'ap_fixed<16,6>'},
- #                       'Dense' : {'Precision' : {'result' : po}},
-  #                      'Conv2D' : {'Precision' : {'result' : po}},
-   #                     'MaxPooling2D' : {'Precision' : po},
-    #                    'BatchNormalization' : {'Precision' : po},
-     #                   'Activation' : {'Precision' : {'result' : po}}}
-  hls_cfg['Model']['PackFactor'] = 1 # an integer that divides the image width with no remained
+  # hls config
+  hls_cfg = {'Model' : {'Precision' : pw}}
+  hls_cfg['LayerName'] = {'output_softmax' : {'Strategy' : 'Stable'}}
+  hls_cfg['LayerType'] = {'Input' : {'Precision' : 'ap_fixed<16,6>'},
+                          'Dense' : {'Precision' : {'result' : po}},
+                          'Conv2D' : {'Precision' : {'result' : po}},
+                          'Pooling2D' : {'Precision' : po},
+                          'BatchNormalization' : {'Precision' : po},
+                          'Activation' : {'Precision' : {'result' : po}}
+                         }
+  hls_cfg['Model']['PackFactor']  = 1
   hls_cfg['Model']['ReuseFactor'] = 1
+  
+  # vivado config
   cfg = hls4ml.converters.create_vivado_config()
-  cfg['IOType'] = 'io_stream'
-  cfg['HLSConfig'] = hls_cfg
-  cfg['KerasModel'] = model # the model
-  cfg['OutputDir'] = model_name.replace(".h5","")+"_bw%i"%(precision) # wherever you want the project to go
+  cfg['IOType']     = 'io_stream'
+  cfg['HLSConfig']  = hls_cfg
+  cfg['KerasModel'] = model
+  cfg['OutputDir']  = model_name.replace(".h5","")+"_bw%i"%(precision) # wherever you want the project to go
   cfg['XilinxPart'] = 'xcvu9p-flgb2104-2l-e'
-  print("Configuration is \n")
   print_dict(hls_cfg)
+  
+  #hls model
   hls_model = hls4ml.converters.keras_to_hls(cfg)
+  for layer in hls_cfg['LayerName'].keys():
+      hls_cfg['LayerName'][layer]['Trace'] = True
   hls_model.compile()
+  hls4ml_pred, hls4ml_trace = hls_model.trace(x_test[:1000])
+  keras_trace = hls4ml.model.profiling.get_ymodel_keras(model, x_test[:1000])
+  y_hls = hls_model.predict(x_test[:1000])
+  
+  for layer in hls_cfg['LayerName'].keys():
+    print("Keras layer {}, first sample:".format(layer))
+    print(keras_trace[layer][0])
+    print("hls4ml layer {}, first sample:".format(layer))
+    print(hls4ml_trace[layer][0])
   
   print('Compile and predict')
-  #x_test = x_test[:100]
-  #y_test = y_test[:100]
+  x_test = x_test[:100]
+  y_test = y_test[:100]
   y_predict        = model    .predict(x_test)
   y_predict_hls4ml = hls_model.predict(x_test)
   print("y_predict = ", y_predict[2])
@@ -68,90 +84,20 @@ def toHLS(model,precision=32):
   print("arg ypredhls = ", np.argmax(y_predict_hls4ml[2]))
   data['accuracy_keras'] = accuracy_score (y_test, np.argmax(y_predict,axis=1))
   data['accuracy_hls4ml'] = accuracy_score(y_test, np.argmax(y_predict_hls4ml,axis=1))
-#  print(y_test- np.argmax(y_predict_hls4ml,axis=1))
-      
+
   print("Accuracy: Keras={} hls4ml={}".format(data['accuracy_keras'],data['accuracy_hls4ml']))
   hls4ml.utils.plot_model(hls_model, show_shapes=True, show_precision=True, to_file='plot_model_{}.png'.format(precision))
 
-  #hls_model.build(csim=False,synth=True, vsynth=True)
-  #hls4ml.utils.plot_model(hls_model, show_shapes=True, show_precision=True, to_file='plot_model_{}.png'.format(precision))
+  hls4ml.utils.plot_model(hls_model, show_shapes=True, show_precision=True, to_file='plot_model_{}.png'.format(precision))
   wp,ap = numerical(keras_model=model, hls_model=hls_model, X=x_test[:1000])
-  #add_logo(ax, fig, 0.14, position='upper right')
-  wp.savefig('%s_profile_weights.pdf'%cfg['OutputDir'])
-  ap.savefig('%s_profile_activations.pdf'%cfg['OutputDir'])
+
+  wp.savefig('%s_profile_weights_LayerTypePrecision.pdf'%cfg['OutputDir'])
+  ap.savefig('%s_profile_activations_LayerTypePrecision.pdf'%cfg['OutputDir'])
   #hls_model.build(csim=False, synth=True, vsynth=True) 
 
-def readReports(indir,p):
-        data = {}
-        data['w']= int(p)
-        # Get the resources from the logic synthesis report 
-        report = open('{}/vivado_synth.rpt'.format(indir))
-        lines = np.array(report.readlines())
-        data['lut'] = int(lines[np.array(['CLB LUTs*' in line for line in lines])][0].split('|')[2])
-        data['ff'] = int(lines[np.array(['CLB Registers' in line for line in lines])][0].split('|')[2])
-        data['bram'] = float(lines[np.array(['Block RAM Tile' in line for line in lines])][0].split('|')[2])
-        data['dsp'] = int(lines[np.array(['DSPs' in line for line in lines])][0].split('|')[2])
-        report.close()
-
-        # Get the latency from the Vivado HLS report
-        report = open('{}/myproject_prj/solution1/syn/report/myproject_csynth.rpt'.format(indir))
-        lines = np.array(report.readlines())
-        lat_line = lines[np.argwhere(np.array(['Latency (clock cycles)' in line for line in lines])).flatten()[0] + 6]
-        data['latency_clks'] = int(lat_line.split('|')[2])
-        data['latency_ns'] = float(lat_line.split('|')[2])*5.0
-        data['latency_ii'] = int(lat_line.split('|')[4])
-        return data
-
-def make_plots(data):
-    print(data)
-    plt.clf()
-    fig,ax = plt.subplots()
-    plt.plot(data['w'], data['dsp'] * 10, label=r'DSP (scaled by x10)', color='#a6611a')
-    plt.plot(data['w'], data['lut'], label=r'LUT', color='#dfc27d')
-    plt.plot(data['w'], data['ff'], label=r'FF', color ='#80cdc1')
-    plt.plot(data['w'], data['bram'] * 100, label=r'BRAM (scaled by x100)', color ='#018571')
-    plt.legend()
-    #plt.figtext(0.125, 0.18,'Post-training quant. (5 ns clock)', wrap=True, horizontalalignment='left',verticalalignment='bottom')
-    #axes = plt.gca()
-    #axes.set_xlim([1.,16])
-    #axes.set_ylim([0,200000])
-    plt.xlabel('Bitwidth')
-    plt.ylabel('Resource Consumption')
-    plt.gca().get_xaxis().set_major_locator(MaxNLocator(integer=True))
-    plt.savefig("scan_resources.pdf")
-
-    plt.clf()
-    fig,ax = plt.subplots()
-    plt.plot(data['w'], data['latency_clks'], label=r'Latency', color='#a6611a')
-    plt.plot(data['w'], data['latency_ii'], label=r'Initiation Interval', color ='#80cdc1')
-    plt.legend()
-    plt.figtext(0.125, 0.18,'Post-training quant. (5 ns clock)', wrap=True, horizontalalignment='left',verticalalignment='bottom')
-    axes = plt.gca()
-    #axes.set_xlim([1.,16])
-    axes.set_ylim([6000,8250])
-    plt.xlabel('Bitwidth')
-    plt.ylabel('Latency (clock cycles)')
-    plt.gca().get_xaxis().set_major_locator(MaxNLocator(integer=True))
-    plt.savefig("scan_latency_cc.pdf")
-
-    plt.clf()
-    fig,ax = plt.subplots()
-    plt.plot(data['w'], data['latency_ns'] / 1000., label=r'Latency', color='#a6611a') 
-    plt.plot(data['w'], 5 * data['latency_ii'] / 1000., label=r'Initiation Interval', color ='#80cdc1')
-    plt.legend()
-    plt.figtext(0.125, 0.18,'Post-training quant. (5 ns clock)', wrap=True, horizontalalignment='left',verticalalignment='bottom')
-    axes = plt.gca()
-    #axes.set_xlim([1.,16])
-    axes.set_ylim([30,45])
-    plt.xlabel('Bitwidth')
-    plt.ylabel('Latency (microseconds)')
-    plt.gca().get_xaxis().set_major_locator(MaxNLocator(integer=True))
-    plt.savefig("scan_latency_ns.pdf")
-
 if __name__ == '__main__':
-    doParallel = int(sys.argv[2])
     model_name = str(sys.argv[1])
-    model = tf.keras.models.load_model("/data/thaarres/hls4ml_docker/hls4ml_cnns/latency_models/"+model_name,custom_objects={'PruneLowMagnitude': pruning_wrapper.PruneLowMagnitude,'QDense': QDense, 'QConv2D': QConv2D, 'Clip': Clip, 'QActivation': QActivation})
+    model = tf.keras.models.load_model("models/"+model_name,custom_objects={'PruneLowMagnitude': pruning_wrapper.PruneLowMagnitude,'QDense': QDense, 'QConv2D': QConv2D, 'Clip': Clip, 'QActivation': QActivation})
     model.summary()
     model  = strip_pruning(model)
     (x_train, y_train), (x_test, y_test) = getNumpyData('svhn_cropped',oneHot=False)
@@ -163,12 +109,10 @@ if __name__ == '__main__':
     precision = [16,14,12,10,8,6,4,3,2,1]
     precision = [16]
     data = {'w':[], 'dsp':[], 'lut':[], 'ff':[], 'bram':[], 'latency_clks':[], 'latency_ns':[], 'latency_ii':[]}
-    if doParallel:
-      Parallel(n_jobs=10, backend='multiprocessing')(delayed(toHLS)(i) for i in precision)
+    #Parallel(n_jobs=10, backend='multiprocessing')(delayed(toHLS)(i) for i in precision)
     #precision = np.flip(precision)
-    else:
-      for p in precision:
-        toHLS(model,p)
+    for p in precision:
+      toHLS(model,p)
     #for p in precision:
    #     datai = readReports(model_name.replace(".h5","")+"_bw%i"%(p),p)
   #      for key in datai.keys():
